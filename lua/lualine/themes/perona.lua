d35479e3b18b12e2fcd760370eaa4d08a89c81dc
spica <sailorfe@proton.me>
2024-12-26 02:04:30 -0500

    update gitignore and lualine

--- lua/lualine/themes/perona.lua
+++ lua/lualine/themes/perona.lua
@@ -2,21 +2,21 @@ local gpp = require("perona.thrillerbark").get()
 local perona = {}
 
 perona.normal = {
-	a = { bg = gpp.cocoa, fg = gpp.ghost },
-	b = { bg = gpp.kuraigana, fg = gpp.ghost },
-	c = { bg = gpp.nagashidaru, fg = gpp.text },
-	x = { bg = gpp.nagashidaru, fg = gpp.ghost },
+	a = { bg = gpp.wine, fg = gpp.hollow },
+	b = { bg = gpp.mihawk, fg = gpp.text },
+	c = { bg = gpp.nightmare, fg = gpp.text },
+	x = { bg = gpp.wine, fg = gpp.ghost },
 }
 perona.inactive = {
-	c = { bg = gpp.coffin, fg = gpp.text },
+	c = { bg = gpp.kuraigana, fg = gpp.text },
 }
 perona.insert = {
 	a = { bg = gpp.surprise, fg = gpp.base },
-	b = { bg = gpp.zombie, fg = gpp.mihawk },
+	b = { bg = gpp.zombie, fg = gpp.base },
 }
 perona.visual = {
 	a = { bg = gpp.wonder, fg = gpp.base },
-	b = { bg = gpp.garden, fg = gpp.mihawk },
+	b = { bg = gpp.garden, fg = gpp.base },
 }
 perona.replace = {
 	a = { bg = gpp.blood, fg = gpp.mihawk },
@@ -28,7 +28,7 @@ perona.terminal = {
 }
 perona.command = {
 	a = { bg = gpp.kumashi, fg = gpp.base },
-	b = { bg = gpp.sea, fg = gpp.mihawk },
+	b = { bg = gpp.sea, fg = gpp.base },
 }
 
 return perona
