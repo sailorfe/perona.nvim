5229555b08e3f8e3746979369254d6db2a77d834
spica <sailorfe@proton.me>
2025-01-12 10:38:08 -0500

    lualine

---
+++ lua/perona/ghost.lua
@@ -0,0 +1,23 @@
+local ghost = {}
+---@return table
+function ghost.get()
+	return {
+		["kuraigana"]	= "#191017",	-- base00
+		["dracule"]		= "#261722",	-- base01
+		["mihawk"]		= "#331f2d",	-- base02
+		["grave"]			=	"#846c81",	-- base03
+		["negative"]	= "#ab87a2",	-- base04
+		["hollow"]		= "#dbb3c8",	-- base05
+		["lace"]			=	"#e5c8d7",	-- base06
+		["cocoa"]			= "#3f2739",	-- base07
+		["ruby"]			= "#d32246",	-- base08
+		["wonder"]		= "#b374be",	-- base09
+		["zoro"]			= "#6ba3be",	-- base0A
+		["jam"]				= "#d65c8d",	-- base0B
+		["fruit"]			= "#eb8490",	-- base0C
+		["garden"]		= "#da95d5",	-- base0D
+		["kumashi"]		= "#acc5d9",	-- base0E
+		["zombie"]		= "#f877b5",	-- base0F
+	}
+end
+return ghost
