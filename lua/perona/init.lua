c9b507bdb06aebd3b583fe94cdbef57bbaf2d4e3
spica <sailorefe@proton.me>
2025-01-24 20:16:01 -0500

    help

--- lua/perona/init.lua
+++ lua/perona/init.lua
@@ -1,18 +1,6 @@
 local GPP = {}
 GPP.name = "perona"
---- @class HoroConfig colorscheme options
-local defaults = {
-  transparent = false, -- set transparent bg hl
-  overrides = false,
-}
-GPP.opts = defaults
---- @param opts HoroConfig colorscheme opts
-function GPP.setup(opts)
-  GPP.opts = vim.tbl_deep_extend("force", {}, GPP.opts or defaults, opts or {})
-end
---- @param opts HoroConfig colorscheme opts
-function GPP.getintherobotshinji(opts)
-	if opts then GPP.setup(opts) end
+function GPP.negativehollow(opts)
 	-- vim.cmd("set t_Co=256")
 	vim.cmd("hi clear")
 	vim.o.termguicolors = true
